45b4424b0947cfe0ac267d241669f1fb6f5063f7
jpinyt <pep.pinyot@gmail.con>
2020-09-23 12:51:59 +0200

    [WIP] correct syntax for comments

--- includes/libmalloc.h
+++ includes/libmalloc.h
@@ -6,22 +6,27 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 10:27:58 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/09/23 11:52:08 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/09/23 12:51:10 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <sys/mman.h>		//mmap, munmap
-#include <sys/resource.h>	//getrlimit
-#include <unistd.h>		//getpagesize
+#ifndef LIBMALLOC_H
+# define LIBMALLOC_H
 
-const size_t k_tiny_max_size = 16;	//n
-const size_t k_small_max_size = 64;	//m
-const size_t k_tiny_size = 16;		//N
-const size_t k_small_size = 64;		//M
+# include <stdlib.h>
+# include <sys/mman.h> /* mmap, munmap */
+# include <sys/resource.h> /* getrlimit */
+# include <unistd.h> /* getpagesize */
+
+/* const size_t k_tiny_max_size = 16; /1* n *1/ */
+/* const size_t k_small_max_size = 64; /1* m *1/ */
+/* const size_t k_tiny_size = 16; /1* N *1/ */
+/* const size_t k_small_size = 64; /1* M *1/ */
 
 void	free(void *ptr);
 void	*malloc(size_t size);
 void	*realloc(void *ptr, size_t size);
 
 void	show_alloc_mem();
+
+#endif
