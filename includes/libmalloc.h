0fc245d3513cfc3cc503b4656741d405de2a14e6
jpinyt <pep.pinyot@gmail.con>
2020-09-23 12:05:23 +0200

    [WIP] getting information

--- includes/libmalloc.h
+++ includes/libmalloc.h
@@ -6,14 +6,19 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 10:27:58 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/09/23 10:40:49 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/09/23 11:52:08 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <sys/mman.h>		//mmap, munmap
+#include <sys/resource.h>	//getrlimit
+#include <unistd.h>		//getpagesize
 
-const size_t k_tiny_max_size = 16;
-const size_t k_small_max_size = 64;
+const size_t k_tiny_max_size = 16;	//n
+const size_t k_small_max_size = 64;	//m
+const size_t k_tiny_size = 16;		//N
+const size_t k_small_size = 64;		//M
 
 void	free(void *ptr);
 void	*malloc(size_t size);
