1df6165ae1f99a8ff28dea18f5e9e42a4502bf22
jpinyt <pep.pinyot@gmail.con>
2020-09-24 10:25:25 +0200

    [WIP] testing t_mem_block struct, no compile because CMake problem

--- includes/libmalloc.h
+++ includes/libmalloc.h
@@ -6,7 +6,7 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 10:27:58 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/09/23 12:51:10 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/09/24 10:19:21 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,55 @@
 # include <sys/mman.h> /* mmap, munmap */
 # include <sys/resource.h> /* getrlimit */
 # include <unistd.h> /* getpagesize */
+# include <pthread.h> /* bonus part */
 
-/* const size_t k_tiny_max_size = 16; /1* n *1/ */
-/* const size_t k_small_max_size = 64; /1* m *1/ */
-/* const size_t k_tiny_size = 16; /1* N *1/ */
-/* const size_t k_small_size = 64; /1* M *1/ */
+/* const size_t k_tiny_size = getpagesize(); /1* N *1/ */
+/* const size_t k_small_size = getpagesize() * 4; /1* M *1/ */
+/* const size_t k_tiny_max_size = k_tiny_size / 2; /1* n *1/ */
+/* const size_t k_small_max_size = k_small_size / 2; /1* m *1/ */
+
+#define TINY_SIZE = getpagesize(); /* N */
+#define SMALL_SIZE = getpagesize() * 4; /* M */
+#define TINY_MAX_SIZE = TINY_SIZE / 2; /* n */
+#define SMALL_MAX_SIZE = SMALL_SIZE / 2; /* m */
+
+const size_t k_allocation_count = 100;
+
+enum	e_zones_type
+{
+	tiny,
+	small,
+	large
+};
+
+typedef struct	s_mem_block
+{
+	void			*addr;
+	size_t			size;
+	enum e_zones_type	zone_type;
+	struct s_mem_block	*left;
+	struct s_mem_block	*right;
+}		t_mem_block;
+
+/* ypedef struct		s_block { */
+/* 	void			*addr; */
+/* 	size_t			size; */
+/* 	struct s_block	*next; */
+/* 	short			zone_type; */
+/* 	char			zone_id; */
+/* 	char			free; */
+/* }					t_block; */
+
+typedef struct	s_env
+{
+	t_mem_block		*tiny;
+	t_mem_block		*small;
+	t_mem_block		*large;
+	/* pthread_mutex_t	lock; */
+	/* int			lock_init; */
+}		t_env;
+
+struct s_env	*k_env;
 
 void	free(void *ptr);
 void	*malloc(size_t size);
